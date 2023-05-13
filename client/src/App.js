9b7bdf724f2dd8c4163cd76aaed6f24b1f206da9
현수 <znight1020@naver.com>
2023-05-13 16:11:06 +0900

    design: HomePage Init

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
+import HomePage from "./component/home/HomePage";
+
 function App() {
     return (
         <div className="App">
             <Routes>
-                <Route path="/" element={<Home />}></Route>
+                <Route path="/" element={<HomePage />}></Route>
             </Routes>
         </div>
     );
