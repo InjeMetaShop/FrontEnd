64d2500d279973d89284a15b3792244c255e14eb
현수 <znight1020@naver.com>
2023-05-13 16:19:03 +0900

    test: Spring과 API 통신 테스트
    - ping 메소드 호출 -> pong! 출력

--- client/src/component/home/HomePage.js
+++ client/src/component/home/HomePage.js
@@ -1,6 +1,12 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import axios from "axios";
 function HomePage() {
+    useEffect(() => {
+        axios.get("/api/ping").then((response) => {
+            console.log(response.data);
+        });
+    }, []);
+
     return <div>HomePage</div>;
 }
 
