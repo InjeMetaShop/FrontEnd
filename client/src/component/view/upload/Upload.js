d9d0dff7c03ba7a52d4409846a3aa951b3c1fe77
현수 <znight1020@naver.com>
2023-06-22 06:42:33 +0900

    feat: 상품 추가 기능 구현

--- client/src/component/view/upload/Upload.js
+++ client/src/component/view/upload/Upload.js
@@ -1,66 +1,68 @@
-import React, { useState } from "react";
-import Dropzone from "react-dropzone";
-import axios from "axios";
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import MenuBar from "../layout/MenuBar";
+import { Paper } from "@mui/material";
+import Box from "@mui/material/Box";
+import UploadDetail from "./UploadDetail";
+
+const defaultTheme = createTheme();
+const mdTheme = createTheme();
 
 export default function Upload() {
-    const [selectedFiles, setSelectedFiles] = useState([]);
-
-    const handleFileSelect = (files) => {
-        setSelectedFiles(files);
-    };
-
-    const handleUpload = () => {
-        if (selectedFiles.length > 0) {
-            const formData = new FormData();
-            selectedFiles.forEach((file) => {
-                formData.append("product", file);
-            });
-
-            axios
-                .post("/api/product/add", formData, {
-                    headers: {
-                        "Content-Type": "multipart/form-data",
-                    },
-                })
-                .then((response) => {
-                    console.log("Files uploaded successfully");
-                    // 파일 업로드 성공 후 처리할 작업 추가
-                })
-                .catch((error) => {
-                    console.error("Error uploading files:", error);
-                    // 파일 업로드 실패 시 처리할 작업 추가
-                });
-        }
-    };
-
     return (
-        <div>
-            <Dropzone
-                onDrop={handleFileSelect}
-                accept=".png, .jpg, .fbx"
-                multiple
-            >
-                {({ getRootProps, getInputProps }) => (
-                    <div {...getRootProps()}>
-                        <input {...getInputProps()} />
-                        <p>
-                            Drag and drop files here, or click to select files
-                        </p>
-                    </div>
-                )}
-            </Dropzone>
-
-            {selectedFiles.length > 0 && (
-                <div>
-                    <p>Selected files:</p>
-                    <ul>
-                        {selectedFiles.map((file, index) => (
-                            <li key={index}>{file.name}</li>
-                        ))}
-                    </ul>
-                    <button onClick={handleUpload}>Upload</button>
-                </div>
-            )}
-        </div>
+        <ThemeProvider theme={defaultTheme}>
+            <CssBaseline />
+            <MenuBar />
+            <ThemeProvider theme={mdTheme}>
+                <Box sx={{ display: "flex" }}>
+                    <Box
+                        component="main"
+                        sx={{
+                            backgroundColor: (theme) =>
+                                theme.palette.mode === "light"
+                                    ? theme.palette.grey[100]
+                                    : theme.palette.grey[900],
+                            flexGrow: 1,
+                            height: "100vh",
+                        }}
+                    >
+                        <div
+                            className="content"
+                            style={{
+                                width: "95%",
+                                margin: "auto",
+                                paddingTop: "1vmax",
+                                paddingLeft: "10vmax",
+                            }}
+                            sx={{ mt: 4, mb: 4 }}
+                        >
+                            <Grid
+                                container
+                                spacing={3}
+                                style={{
+                                    width: "100%",
+                                }}
+                                paddingTop="3vmax"
+                            >
+                                <Grid item xs={12}>
+                                    <Paper
+                                        sx={{
+                                            p: 2,
+                                            display: "flex",
+                                            flexDirection: "column",
+                                            height: 900,
+                                        }}
+                                    >
+                                        <UploadDetail />
+                                    </Paper>
+                                </Grid>
+                            </Grid>
+                        </div>
+                    </Box>
+                </Box>
+            </ThemeProvider>
+        </ThemeProvider>
     );
 }
---
+++ client/src/component/view/upload/UploadDetail.js
@@ -0,0 +1,241 @@
+import React, { useState } from "react";
+import Grid from "@mui/material/Grid";
+import Dropzone from "react-dropzone";
+import axios from "axios";
+
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
+export default function UploadDetail() {
+    const [selectedImg, setSelectedImg] = useState([]);
+    const [selectedFbx, setSelectedFbx] = useState([]);
+    const [productDetails, setProductDetails] = useState({
+        name: "상품이름",
+        price: 0,
+        sex: "man",
+        category: "up",
+        approve: "false",
+    });
+    const [previewImage, setPreviewImage] = useState(null);
+    const [previewMessage, setPreviewMessage] = useState(
+        "이곳에 3D 모델 파일(FBX)을 넣어주세요"
+    );
+
+    const handleProductNameChange = (event) => {
+        const updatedProductDetails = {
+            ...productDetails,
+            name: event.target.value,
+        };
+        setProductDetails(updatedProductDetails);
+    };
+
+    const handleProductPriceChange = (event) => {
+        const updatedProductDetails = {
+            ...productDetails,
+            price: event.target.value,
+        };
+        setProductDetails(updatedProductDetails);
+    };
+
+    const handleProductSexChange = (event) => {
+        const updatedProductDetails = {
+            ...productDetails,
+            sex: event.target.value,
+        };
+        setProductDetails(updatedProductDetails);
+    };
+
+    const handleProductCategoryChange = (event) => {
+        const updatedProductDetails = {
+            ...productDetails,
+            category: event.target.value,
+        };
+        setProductDetails(updatedProductDetails);
+    };
+
+    const handleImgSelect = (files) => {
+        setSelectedImg(files);
+
+        const file = files[0];
+        const reader = new FileReader();
+
+        reader.onload = () => {
+            setPreviewImage(reader.result);
+        };
+
+        reader.readAsDataURL(file);
+    };
+
+    const handleFbxSelect = (files) => {
+        setSelectedFbx(files);
+        setPreviewMessage(
+            "파일 업로드 완료. 3D 객체는 미리보기가 제공되지 않습니다."
+        );
+    };
+
+    const handleUpload = () => {
+        if (selectedImg.length > 0 && selectedFbx.length > 0) {
+            console.log(selectedImg[0]);
+            console.log(selectedFbx[0]);
+            const formData = new FormData();
+
+            formData.append("product", JSON.stringify(productDetails));
+            formData.append("thumbnail", selectedImg[0]);
+            formData.append("fbxFile", selectedFbx[0]);
+
+            axios
+                .post("/api/product/add", formData, {
+                    headers: {
+                        "Content-Type": "multipart/form-data",
+                    },
+                })
+                .then((response) => {
+                    console.log("Files uploaded successfully");
+                    // 파일 업로드 성공 후 처리할 작업 추가
+                })
+                .catch((error) => {
+                    console.error("Error uploading files:", error);
+                    // 파일 업로드 실패 시 처리할 작업 추가
+                });
+        }
+    };
+
+    return (
+        <div>
+            <Grid container spacing={2}>
+                <Grid item xs={6}>
+                    <Dropzone onDrop={handleImgSelect} accept=".png, .jpg">
+                        {({ getRootProps, getInputProps, isDragActive }) => (
+                            <div
+                                {...getRootProps()}
+                                style={{
+                                    border: isDragActive
+                                        ? "2px dashed blue"
+                                        : "2px dashed gray",
+                                    padding: "20px",
+                                    textAlign: "center",
+                                }}
+                            >
+                                <input {...getInputProps()} />
+                                {previewImage ? (
+                                    <img
+                                        src={previewImage}
+                                        alt="Preview"
+                                        style={{
+                                            maxWidth: "100%",
+                                            maxHeight: "200px",
+                                        }}
+                                    />
+                                ) : (
+                                    <p>
+                                        {isDragActive
+                                            ? "Drop the image files here..."
+                                            : "이곳에 미리보기 용도의 이미지 파일을 넣어주세요."}
+                                    </p>
+                                )}
+                            </div>
+                        )}
+                    </Dropzone>
+                </Grid>
+                <Grid item xs={6}>
+                    <Dropzone onDrop={handleFbxSelect} accept=".fbx">
+                        {({ getRootProps, getInputProps, isDragActive }) => (
+                            <div
+                                {...getRootProps()}
+                                style={{
+                                    border: isDragActive
+                                        ? "2px dashed blue"
+                                        : "2px dashed gray",
+                                    padding: "20px",
+                                    textAlign: "center",
+                                }}
+                            >
+                                <input {...getInputProps()} />
+                                <p>{previewMessage}</p>
+                            </div>
+                        )}
+                    </Dropzone>
+                </Grid>
+            </Grid>
+            <br />
+            <br />
+            <br />
+
+            <Box
+                component="form"
+                sx={{
+                    "& > :not(style)": { m: 1, width: "25ch" },
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                <TextField
+                    id="standard-basic"
+                    label="상품 이름"
+                    variant="standard"
+                    onChange={handleProductNameChange}
+                />
+                <TextField
+                    id="standard-basic"
+                    label="상품 가격"
+                    variant="standard"
+                    onChange={handleProductPriceChange}
+                />
+
+                <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                    <InputLabel id="demo-simple-select-standard-label">
+                        성별
+                    </InputLabel>
+                    <Select
+                        labelId="demo-simple-select-standard-label"
+                        id="demo-simple-select-standard"
+                        value={productDetails.sex}
+                        onChange={handleProductSexChange}
+                        label="Sex"
+                    >
+                        <MenuItem value={"man"}>man</MenuItem>
+                        <MenuItem value={"woman"}>woman</MenuItem>
+                    </Select>
+                </FormControl>
+
+                <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                    <InputLabel id="demo-simple-select-standard-label">
+                        카테고리
+                    </InputLabel>
+                    <Select
+                        labelId="demo-simple-select-standard-label"
+                        id="demo-simple-select-standard"
+                        value={productDetails.category}
+                        onChange={handleProductCategoryChange}
+                        label="Category"
+                    >
+                        <MenuItem value={"up"}>up</MenuItem>
+                        <MenuItem value={"down"}>down</MenuItem>
+                        <MenuItem value={"cap"}>cap</MenuItem>
+                        <MenuItem value={"Accessory"}>accessory</MenuItem>
+                    </Select>
+                </FormControl>
+            </Box>
+
+            {(selectedImg.length > 0 || selectedFbx.length > 0) && (
+                <div style={{ textAlign: "left" }}>
+                    <h3>선택된 파일</h3>
+                    <ul>
+                        {selectedImg.map((file, index) => (
+                            <li key={index}>{file.name}</li>
+                        ))}
+                        {selectedFbx.map((file, index) => (
+                            <li key={index}>{file.name}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+            <button onClick={handleUpload}>Upload</button>
+        </div>
+    );
+}
